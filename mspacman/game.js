678ca0d3c3367a09f2715997928c0a2db2328909
James McDowell <james.mcdowell@tufts.edu>
2015-03-02 18:04:58 -0500

    wrote preliminary javascript

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,8 @@
 // Replace this with your code...
+
+var canvas = document.getElementById("game_canvas");
+var ctx = canvas.getContext("2d");
+var image = document.getElementById('pacman10-hp-sprite.png');
+
+void ctx.drawImage(pacman10-hp-sprite.png, 322, 0, 464, 135);
+void ctx.drawImage(pacman10-hp-sprite.png, 83, 23, 13, 14);
