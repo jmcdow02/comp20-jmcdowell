81ac55c173d2dbc7dbe8f27ddb04981933c04939
James McDowell <james.mcdowell@tufts.edu>
2015-03-05 23:04:45 -0500

    placed ms pacman on board

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,8 +1,13 @@
-// Replace this with your code...
+function init() {
+    var canvas = document.getElementById('game_canvas');
+    var ctx = canvas.getContext("2d");
+    var image = new Image();
+    image.src = 'pacman10-hp-sprite.png';
 
-var canvas = document.getElementById("game_canvas");
-var ctx = canvas.getContext("2d");
-var image = document.getElementById('pacman10-hp-sprite.png');
-
-void ctx.drawImage(pacman10-hp-sprite.png, 322, 0, 464, 135);
-void ctx.drawImage(pacman10-hp-sprite.png, 83, 23, 13, 14);
+    image.addEventListener("load", function() {
+    	//board: start at 322 to the right, 0 down. image is 464x135
+    	ctx.drawImage(image, 322, 0, 463, 138, 230, 0, 463, 138);
+    	//ms pacman: start 83 to the right, 23 down. image is 13x14
+    	ctx.drawImage(image, 84, 23, 17, 17, 268, 30, 17, 17);
+    }, false);
+}
