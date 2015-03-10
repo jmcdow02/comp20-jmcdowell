3136d5b891bcb64b0e90ef781d38038464514646
James McDowell <james.mcdowell@tufts.edu>
2015-03-10 12:06:01 -0400

    tried to branch

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,27 @@
-// Your JavaScript goes here...
+function parse() {
+	//create instance of object
+	request = new XMLHttpRequest();
+	//create or "open" HTTP request
+	request.open("GET", "data.json", true);//true if asynchronous
+	//set up way to manage response -- to a function
+	request.onreadystatechange = parseData;
+	//execute the request
+	request.send();
+	console.log("it sent");
+}
+function parseData() {
+	console.log("can you read this?");
+	//check the state
+	if (request.readyState == 4) {
+		//get the 'messages' part of the document
+		messagesPart = document.getElementById("messages");
+		//parses the text
+		parsed = JSON.parse(request.responseText);
+		//loops through length of text
+		for (i = 0; i < parsed.length; i++) {
+			messagesPart.innerHTML += "<p>" 
+			+ converted[i]["content"] + " " 
+			+ converted[i]['username'] + "</p>";
+		}
+	}
+}
